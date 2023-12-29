ae1172f6a0fa8ba15f75cd39878090a61bfccb1f
NDH <nick@ndhrones.com>
2023-12-29 09:51:20 -0500

    Update kvProcedures.ts

--- kvProcedures.ts
+++ kvProcedures.ts
@@ -81,14 +81,14 @@ export async function getAllKeys() {
 export async function loadTestSet() {
    if (!db) await initDB()
    await db.set(["env", "cwd"], "./")
-   // await db.set(["env", "host"], "http://localhost")
-   // await db.set(["env", "port"], 9099)
-   // await db.set(["cfg", "target"], "./dist")
-   // await db.set(["cfg", "include"], "./src")
-   // await db.set(["cfg", "options"], { debug: true, useKv: true, dbFile: "./data/db.db" })
-   // await db.set(["users", 1], { id: 1, first: "John", last: "Doe", age: 25, address: { street: '123 Main st.', city: 'Gotham', state: "CA", zip: 45927 } })
-   // await db.set(["users", 2], { id: 2, first: "Jim", last: "Smith", age: 35, address: { street: '456 A st.', city: 'Fremont', state: "CA", zip: 45938 } })
-   // await db.set(["users", 3], { id: 3, first: "Joe", last: "Smoe", age: 45, address: { street: '789 B st.', city: 'Hayward', state: "CA", zip: 45941 } })
+   await db.set(["env", "host"], "http://localhost")
+   await db.set(["env", "port"], 9099)
+   await db.set(["cfg", "target"], "./dist")
+   await db.set(["cfg", "include"], "./src")
+   await db.set(["cfg", "options"], { debug: true, useKv: true, dbFile: "./data/db.db" })
+   await db.set(["users", 1], { id: 1, first: "John", last: "Doe", age: 25, address: { street: '123 Main st.', city: 'Gotham', state: "CA", zip: 45927 } })
+   await db.set(["users", 2], { id: 2, first: "Jim", last: "Smith", age: 35, address: { street: '456 A st.', city: 'Fremont', state: "CA", zip: 45938 } })
+   await db.set(["users", 3], { id: 3, first: "Joe", last: "Smoe", age: 45, address: { street: '789 B st.', city: 'Hayward', state: "CA", zip: 45941 } })
 }
 
 
